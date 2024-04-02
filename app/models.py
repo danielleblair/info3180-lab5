d5305727d56cec3708f47b5b5c4a29f9e70a88bc
Danielle Blair <danielle2blair@gmail.com>
2024-04-02 14:31:57 -0500

    Added movies route

--- app/models.py
+++ app/models.py
@@ -1,3 +1,42 @@
 
 # Add any model classes for Flask-SQLAlchemy here
 
+from . import db
+from werkzeug.security import generate_password_hash
+
+from sqlalchemy.sql import func
+
+
+class Movie(db.Model):
+    
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.TextArea(300))
+    poster = db.Column(db.String(128))
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+
+    def __init__(self, title, description, poster,  created_at):
+        self.title = title
+        self.description = description
+        self.username = poster
+        self. created_at =  created_at
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2 support
+        except NameError:
+            return str(self.id)  # python 3 support
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
--- app/views.py
+++ app/views.py
@@ -1,13 +1,10 @@
-"""
-Flask Documentation:     https://flask.palletsprojects.com/
-Jinja2 Documentation:    https://jinja.palletsprojects.com/
-Werkzeug Documentation:  https://werkzeug.palletsprojects.com/
-This file creates your application.
-"""
-
-from app import app
-from flask import render_template, request, jsonify, send_file
 import os
+from app import app, db
+from flask import render_template, request, jsonify, send_file, redirect, url_for, flash, session, abort, send_from_directory
+from werkzeug.utils import secure_filename
+from app.models import Movie
+from app.forms import MovieForm
+
 
 
 ###
@@ -19,12 +16,60 @@ def index():
     return jsonify(message="This is the beginning of our API")
 
 
+
+@app.route('/about/')
+def about():
+    """Render the website's about page."""
+    return render_template('about.html', name="Mary Jane")
+
+
+@app.route('/api/v1/movies', methods=['POST'])
+def movies():
+    """displaying the form to add a new movie."""
+    mform = MovieForm()
+
+    if mform.validate_on_submit():
+
+        p_title = mform.title.data
+        p_description = mform.description.data
+
+        p_poster = mform.poster.data
+
+        filename = secure_filename(p_poster.filename)
+        p_poster.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        movie = Movie(title= p_title, description=p_description, filename=filename)
+        db.session.add(movie)
+        db.session.commit()
+
+        flash('Movie Created and Saved', 'success')
+        return jsonify({
+                "message": "Movie Successfully added",
+                "title": movie.title,
+                "poster": movie.poster,
+                "description": movie.description
+        })
+
+    else:
+        return jsonify({'errors': form_errors(mform)})
+
+
+def get_uploaded_images():
+    import os
+    rootdir = os.getcwd()
+    fileslst = []
+    for subdir, dirs, files in os.walk(rootdir + '/uploads'):
+        for file in files:
+            #print os.path.join(subdir, file)
+            fileslst.append(file)
+    return fileslst
+   
+        
+
 ###
 # The functions below should be applicable to all Flask apps.
 ###
 
-# Here we define a function to collect form errors from Flask-WTF
-# which we can later use
 def form_errors(form):
     error_messages = []
     """Collects form errors"""
@@ -38,6 +83,15 @@ def form_errors(form):
 
     return error_messages
 
+# Flash errors from the form if validation fails
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" % (
+                getattr(form, field).label.text,
+                error
+), 'danger')
+
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
     """Send your static text file."""
@@ -49,8 +103,7 @@ def send_text_file(file_name):
 def add_header(response):
     """
     Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also tell the browser not to cache the rendered page. If we wanted
-    to we could change max-age to 600 seconds which would be 10 minutes.
+    and also to cache the rendered page for 10 minutes.
     """
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
     response.headers['Cache-Control'] = 'public, max-age=0'
